188d7d4b43a7133d76b7047180154ab3925aef5d
Seyed Emad Hosseini <irc.master@hotmail.com>
2022-07-17 12:29:33 +0430

    Initial Commit of Infrastructure.Data Project, Update MVC For new UniversityDBContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UnivesityDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UnivesityDBContext: DbContext
+    {
+        public UnivesityDBContext(DbContextOptions options): base (options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
--- CleanArch/CleanArch.Mvc/Program.cs
+++ CleanArch/CleanArch.Mvc/Program.cs
@@ -1,3 +1,4 @@
+using CleanArch.Infra.Data.Context;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,10 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<UnivesityDBContext>(opetions =>
+{
+    opetions.UseSqlServer(builder.Configuration.GetConnectionString("UnivesityDBConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
