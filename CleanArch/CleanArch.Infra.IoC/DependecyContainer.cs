a7047f1ff7c2334caebc3e6d9f57a44584845c20
Seyed Emad Hosseini <irc.master@hotmail.com>
2022-07-18 11:15:55 +0430

    Last Updated

---
+++ CleanArch/CleanArch.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependecyContainer
+    {
+       // public static void RegisterSevises(IServiceCollection services)
+    }
+}
