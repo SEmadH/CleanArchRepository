12433b540d939070361212278b17721d3d9364e0
Seyed Emad Hosseini <irc.master@hotmail.com>
2022-07-23 12:46:05 +0430

    CommandHandlers, Commands, UpdateCourse Repository, Cleaned Up IOC

--- CleanArch/CleanArch.Infra.IoC/DependecyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependecyContainer.cs
@@ -1,13 +1,19 @@
-﻿using CleanArch.Application.Intefaces;
+﻿using CleanArc.Infra.Bus;
+using CleanArch.Application.Intefaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 using CleanArch.Infra.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArch.Domain.CommandHandler;
+using CleanArch.Domain.Commands;
+using CleanArch.Infra.Data.Context;
 
 namespace CleanArch.Infra.IoC
 {
@@ -15,11 +21,18 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterSevices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemmoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UnivesityDBContext>();
         }
     }
 }
